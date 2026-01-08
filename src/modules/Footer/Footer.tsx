40683b8f6af570312a1b36b48805ca154e6665fa
chiriuy <christian@prezly.com>
2026-01-08 16:40:10 -0300

    eneco footer

--- src/modules/Footer/Footer.tsx
+++ src/modules/Footer/Footer.tsx
@@ -1,33 +1,11 @@
 import type { Locale } from '@prezly/theme-kit-nextjs';
-import { translations } from '@prezly/theme-kit-nextjs';
-
-import { app, intl } from '@/adapters/server';
-
-import { CookieConsentLink } from '../CookieConsent';
 
 import * as ui from './ui';
-
-import styles from './ui/Footer.module.scss';
 
 interface Props {
     localeCode: Locale.Code;
 }
 
 export async function Footer({ localeCode }: Props) {
-    const { formatMessage } = await intl(localeCode);
-
-    const newsroom = await app().newsroom();
-
-    return (
-        <ui.Footer isWhiteLabeled={newsroom.is_white_labeled}>
-            <ui.DataRequestLink className={styles.link} newsroom={newsroom} localeCode={localeCode}>
-                {formatMessage(translations.actions.privacyRequests)}
-            </ui.DataRequestLink>
-            <CookieConsentLink
-                className={styles.link}
-                startUsingCookiesLabel={formatMessage(translations.actions.startUsingCookies)}
-                stopUsingCookiesLabel={formatMessage(translations.actions.stopUsingCookies)}
-            />
-        </ui.Footer>
-    );
+    return <ui.Footer />;
 }
--- src/modules/Footer/ui/Footer.tsx
+++ src/modules/Footer/ui/Footer.tsx
@@ -1,33 +1,52 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import type { ReactNode } from 'react';
-
-import { MadeWithPrezly } from '@/components/MadeWithPrezly';
-import { parseBoolean } from '@/utils';
-
 import styles from './Footer.module.scss';
 
-interface Props {
-    children: ReactNode;
-    isWhiteLabeled: boolean;
-}
-
-export function Footer({ children, ...props }: Props) {
-    const searchParams = useSearchParams();
-    const isPreviewMode = process.env.PREZLY_MODE === 'preview';
-
-    let { isWhiteLabeled } = props;
-    if (isPreviewMode && searchParams.has('is_white_labeled')) {
-        isWhiteLabeled = parseBoolean(searchParams.get('is_white_labeled'));
-    }
-
+export function Footer() {
     return (
         <footer className={styles.container}>
             <div className="container">
-                <div className={styles.footer}>
-                    <div className={styles.links}>{children}</div>
-                    {!isWhiteLabeled && <MadeWithPrezly />}
+                <div className={styles.columns}>
+                    {/* Column 1: Logo */}
+                    <div className={styles.logoColumn}>
+                        <a href="https://eneco.be/" className={styles.logo}>
+                            <img
+                                src="https://cdn.uc.assets.prezly.com/361cba53-d084-46b0-bc28-7ddb5c833c06/"
+                                alt="Eneco"
+                                className={styles.logoImage}
+                            />
+                        </a>
+                    </div>
+
+                    {/* Columns 2-5: Links */}
+                    <div className={styles.linksWrapper}>
+                        <div className={styles.linksColumn}>
+                            <a
+                                href="https://cdn.eneco.be/downloads/nl/general/vw/algemene-voorwaarden.pdf"
+                                className={styles.link}
+                            >
+                                Algemene voorwaarden
+                            </a>
+                        </div>
+
+                        <div className={styles.linksColumn}>
+                            <a href="https://eneco.be/nl/disclaimer" className={styles.link}>
+                                Disclaimer
+                            </a>
+                        </div>
+
+                        <div className={styles.linksColumn}>
+                            <a href="https://eneco.be/nl/privacybeleid" className={styles.link}>
+                                Privacy en cookie statement
+                            </a>
+                        </div>
+
+                        <div className={styles.linksColumn}>
+                            <a href="https://eneco.be/nl/cookies" className={styles.link}>
+                                Cookies
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
